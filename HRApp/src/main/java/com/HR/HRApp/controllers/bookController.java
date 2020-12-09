fd6ff13178ec6fff13bb0464f6612ae48412bd5b
Sarah Derr <scd5325@psu.edu>
2020-12-09 01:26:26 -0500

    updating links and html

---
+++ HRApp/src/main/java/com/HR/HRApp/controllers/bookController.java
@@ -0,0 +1,15 @@
+package com.HR.HRApp.controllers;
+
+import com.HR.HRApp.HrAppApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class bookController {
+
+    @RequestMapping("/book")
+    public String about() {
+        return "book.html";
+    }
+}
