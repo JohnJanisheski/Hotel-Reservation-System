1ea3d17eb09bdd4d35797f08aaa519ba7d641d28
John <johnjanisheski99@gmail.com>
2020-12-08 22:08:42 -0500

    Fixed Room and RoomType to be working together now. Added code in bootstrap to add base rooms and admin accounts.

--- HRApp/src/main/java/com/HR/HRApp/bootstrap/bootStrapData.java
+++ HRApp/src/main/java/com/HR/HRApp/bootstrap/bootStrapData.java
@@ -1,10 +1,6 @@
 package com.HR.HRApp.bootstrap;
 
 import com.HR.HRApp.domain.Account;
-//import com.HR.HRApp.domain.manager;
-//import com.HR.HRApp.domain.customer;
-//import com.HR.HRApp.repositories.customerRepository;
-//import com.HR.HRApp.repositories.managerRepository;
 import com.HR.HRApp.domain.room;
 import com.HR.HRApp.domain.roomType;
 import com.HR.HRApp.repositories.accountRepository;
@@ -13,6 +9,7 @@ import com.HR.HRApp.repositories.roomTypeRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
+import java.util.ArrayList;
 
 @EnableJpaRepositories("com.HR.HRApp.repositories")
 @Component
@@ -35,32 +32,63 @@ public class bootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        //Adding Room Types
+        ArrayList<roomType> rt = new ArrayList<roomType>();
+        rt.add(new roomType("Single Smoking", "Image URL", 120.00, 1, true));
+        rt.add(new roomType("Single Non Smoking", "Image URL", 100.00, 1, false));
+        rt.add(new roomType("Double Smoking", "Image URL", 170.00, 2, true));
+        rt.add(new roomType("Double Non Smoking", "Image URL", 150, 2, false));
+        roomTypeRepository.saveAll(rt);
 
-        //room and roomtype
-        room r1 =  new room();
-        roomType rt1 = new roomType();
-        r1.setRoomType(rt1);
-        rt1.getRooms().add(r1);
+        //Adding Rooms
+        ArrayList<room> r = new ArrayList<room>();
+        r.add(new room("101", "Single Bed Room Non Smoking", "Image URL", rt.get(1), 1, 1,true ));
+        r.add(new room("102", "Single Bed Room Non Smoking", "Image URL", rt.get(1), 1, 1,true ));
+        r.add(new room("103", "Single Bed Room Non Smoking", "Image URL", rt.get(1), 1, 1,true ));
+        r.add(new room("104", "Single Bed Room Non Smoking", "Image URL", rt.get(1), 1, 1,true ));
+        r.add(new room("105", "Single Bed Room Smoking", "Image URL", rt.get(0), 1, 1,true ));
+        r.add(new room("106", "Single Bed Room Smoking", "Image URL", rt.get(0), 1, 1,true ));
+        r.add(new room("107", "Single Bed Room Smoking", "Image URL", rt.get(0), 1, 1,true ));
+        r.add(new room("108", "Single Bed Room Smoking", "Image URL", rt.get(0), 1, 1,true ));
+        r.add(new room("201", "Double Bed Room Non Smoking", "Image URL", rt.get(3), 1, 2,true ));
+        r.add(new room("202", "Double Bed Room Non Smoking", "Image URL", rt.get(3), 1, 2,true ));
+        r.add(new room("203", "Double Bed Room Smoking", "Image URL", rt.get(2), 1, 2,true ));
+        r.add(new room("204", "Double Bed Room  Smoking", "Image URL", rt.get(2), 1, 2,true ));
+        for (room room: r){
+            roomRepository.save(room);
+            //Add the room to the roomType
+            if (room.getRemark().contains("Single")){
+                if(room.getRemark().contains("Non"))
+                    rt.get(1).addRoom(room);
+                else
+                    rt.get(0).addRoom(room);
+            }
+            else{
+                if(room.getRemark().contains("Non"))
+                    rt.get(3).addRoom(room);
+                else
+                    rt.get(2).addRoom(room);
+            }
+        }
+        roomTypeRepository.saveAll(rt);
 
         //Admin Accounts and Non Admin Accounts
-        Account acc = new Account(2, "John", "Janisheski", "johnjanisheski99@gmail.com", "AdminPassword");
-        accountRepository.save(acc);
-        acc = new Account(2, "Xinyi", "Peng", "theapxy@gmail.com", "AdminPassword");
-        accountRepository.save(acc);
-        acc = new Account(2, "Sarah", "Derr", "test@gmail.com", "AdminPassword");
-        accountRepository.save(acc);
-        acc = new Account(2, "Yuhui", "Last Name", "kkfiner@gmail.com", "AdminPassword");
-        accountRepository.save(acc);
-        acc = new Account(0, "John", "Doe", "JohnDoe@gmail.com", "CustomerPassword");
-        accountRepository.save(acc);
-        acc = new Account(1, "John", "Doe", "JohnDoe@gmail.com", "StaffPassword");
-        accountRepository.save(acc);
-
+        ArrayList<Account> acc = new ArrayList<Account>();
+        acc.add(new Account(2, "John", "Janisheski", "johnjanisheski99@gmail.com", "AdminPassword"));
+        acc.add(new Account(2, "Xinyi", "Peng", "theapxy@gmail.com", "AdminPassword"));
+        acc.add(new Account(2, "Sarah", "Derr", "test@gmail.com", "AdminPassword"));
+        acc.add(new Account(2, "Yuhui", "Last Name", "kkfiner@gmail.com", "AdminPassword"));
+        acc.add(new Account(0, "John", "Doe", "JohnDoe@gmail.com", "CustomerPassword"));
+        acc.add(new Account(1, "John", "Doe", "JohnDoe@gmail.com", "StaffPassword"));
+        accountRepository.saveAll(acc);
 
         System.out.println("Opened in BootStrap");
         System.out.println("Number of accounts = " + accountRepository.count());
         System.out.println("Number of rooms = " + roomRepository.count());
         System.out.println("Number of room types = " + roomTypeRepository.count());
+//        accountRepository.findAll().forEach(account -> {
+//            System.out.println(account.getEmail());
+//        });
     }
 
     public boolean isLoggedIn() {
